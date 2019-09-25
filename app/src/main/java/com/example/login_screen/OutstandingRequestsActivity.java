4cdf4aaf1ea3a5cf6692e78652e9e763ee5eee99
Silvio <ss2107@kent.ac.uk>
2019-09-25 19:05:06 +0530

    third commit

--- app/src/main/java/com/example/login_screen/OutstandingRequestsActivity.java
+++ app/src/main/java/com/example/login_screen/OutstandingRequestsActivity.java
@@ -2,13 +2,18 @@ package com.example.login_screen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OutstandingRequestsActivity extends AppCompatActivity {
 
     int tot_requests = 0;
     String next_appointment = "NO UPCOMING" + "\n" + "APPOINTMENTS";
+    String appointment_list = "NO UPCOMING" + "\n" + "APPOINTMENTS";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +22,29 @@ public class OutstandingRequestsActivity extends AppCompatActivity {
 
         final TextView counter_requests = (TextView) findViewById(R.id.request_counter);
         final TextView branch_appointment = (TextView) findViewById(R.id.branch_appointment);
+        final TextView new_requests = (TextView) findViewById(R.id.new_requests);
+        final ImageView carousel = (ImageView ) findViewById(R.id.outstanding_request_carousel);
 
         counter_requests.setText(Integer.toString(tot_requests));
         branch_appointment.setText(next_appointment);
+        new_requests.setText(appointment_list);
 
+        carousel.setOnTouchListener(new OnSwipeTouchListener(OutstandingRequestsActivity.this) {
+            public void onSwipeTop() {
+                //Toast.makeText(OutstandingRequestsActivity.this, "top", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeRight() {
+                //Toast.makeText(OutstandingRequestsActivity.this, "right", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeLeft() {
+                Intent mIntent = new Intent(
+                        OutstandingRequestsActivity.this,
+                        CreditCardHome.class);
+                startActivity(mIntent);
+            }
+            public void onSwipeBottom() {
+                //Toast.makeText(OutstandingRequestsActivity.this, "bottom", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
